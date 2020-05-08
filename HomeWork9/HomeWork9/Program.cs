53a34606b1cc828030f47d7a76cb15d64268bd86
Ivica Janevski <janevski.ivica@yahoo.com>
2020-05-08 15:28:52 +0200

    HomeWork 9 task1

---
+++ HomeWork9/HomeWork9/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneBookUser john = new PhoneBookUser( 1,"John",070632985);
+            PhoneBookUser mike = new PhoneBookUser(2, "Mike", 065638782);
+            PhoneBookUser jack = new PhoneBookUser(3, "Jack", 070222782);
+            PhoneBookUser bill = new PhoneBookUser(4, "Bill", 073772772);
+            PhoneBookUser villiam = new PhoneBookUser(5,"Villiam",075687687);
+
+            Dictionary<int, PhoneBookUser> phoneBook = new Dictionary<int, PhoneBookUser>();
+            phoneBook.Add(john.ID,john);
+            phoneBook.Add(mike.ID,mike);
+            phoneBook.Add(jack.ID,jack);
+            phoneBook.Add(bill.ID, jack);
+            phoneBook.Add(villiam.ID,villiam);
+
+            PhoneBookUser John = phoneBook[1];
+            PhoneBookUser Mike = phoneBook[2];
+            PhoneBookUser Jack = phoneBook[3];
+            PhoneBookUser Bill = phoneBook[4];
+            PhoneBookUser Villiam = phoneBook[5];
+
+            Console.WriteLine("Enter name");
+            string userImput = Console.ReadLine();
+            bool noName = false;
+            foreach (KeyValuePair<int, PhoneBookUser> keyValue in phoneBook)
+            {
+                PhoneBookUser user = keyValue.Value;
+                if (userImput == user.Name)
+                {
+                    Console.WriteLine($"User {user.Name} phone number is {user.PhoneNumber} ");
+                    noName = true;
+                }
+            }
+            if (!noName)
+            {
+                Console.WriteLine("Error....no such name");
+            }
+            
+
+
+            //Console.WriteLine($"User ID is :{John.ID} user Name is : {John.Name} userPhomeNumber is {John.PhoneNumber}");
+
+
+            //foreach (KeyValuePair<int,PhoneBookUser> keyValuePair in phoneBook)
+            //{
+            //    Console.WriteLine("KeyValue{0}",keyValuePair.Key);
+            //    PhoneBookUser user = keyValuePair.Value;
+            //    Console.WriteLine($"User ID is :{user.ID} User Name :{user.Name}, User Phone Number {user.PhoneNumber}");
+            //    Console.WriteLine("---------------------------------------------------------------------------------------");
+            //}
+            Console.ReadLine();
+        }
+        public class PhoneBookUser
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public int PhoneNumber { get; set; }
+
+            public PhoneBookUser(int id,string name,int phoneNumber)
+            {
+                ID = id;
+                Name = name;
+                PhoneNumber = phoneNumber;
+            }
+        }
+
+    }
+}
