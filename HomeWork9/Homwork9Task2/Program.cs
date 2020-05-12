ef8b369579505ac5b3afe54bd65d28160e85c390
Ivica Janevski <janevski.ivica@yahoo.com>
2020-05-12 15:14:13 +0200

    addedTask2

---
+++ HomeWork9/Homwork9Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Homwork9Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<int> listOfNumbers = new Queue<int>();
+            bool ifYes = true;
+            while (ifYes)
+            {
+                Console.WriteLine("Do you whant to enter number? Y/N");
+                string user = Console.ReadLine();
+
+                if (user == "y" || user == "Y")
+                {
+                    Console.WriteLine("Enter number");
+                    int userNumber = Convert.ToInt32(Console.ReadLine());
+                    listOfNumbers.Enqueue(userNumber);
+                }
+                else if ((user == "n" || user == "N") || user != "")
+                {
+                    break;
+                }
+                else
+                {
+                    ifYes = true;
+                }
+            }
+            IterateCollectoin(listOfNumbers);
+            Console.ReadLine();
+        }
+        public static void IterateCollectoin(IEnumerable colection)
+        {
+            Console.WriteLine($"The numbers in the Q are :");
+            foreach (var item in colection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
